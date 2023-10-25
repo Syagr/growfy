9c8c4e2dcda40838990afb589a2e200e6f82d6c4
Syagr <114944986+Syagr@users.noreply.github.com>
2023-10-25 14:11:20 +0300

    header ready and fix

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict"
+
+document.addEventListener('click', documentClick);
+
+function documentClick(e) {
+	const target = e.target;
+	if (target.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+}
